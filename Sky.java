25f496301f2f67b7a061c2a4bb8423ff6cf86b47
Daryl <darylnoupik@gmail.com>
2019-05-04 00:52:00 +0100

    Un truc

---
+++ Sky.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Image;
+
+public class Sky implements IArea {
+	private Dimension dimension;
+
+	public Sky (Dimension dimension) {
+		this.dimension = dimension ;
+	}
+	public Dimension getDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
