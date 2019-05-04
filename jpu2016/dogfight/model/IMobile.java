d6f2a3812196c678b88f4e061c2bba7d35b0d24e
Daryl <darylnoupik@gmail.com>
2019-05-04 21:58:07 +0100

    Phase 2 Model

---
+++ jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,32 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Image;
+import java.awt.Point;
+
+public interface IMobile {
+	public Direction getDirection();
+	public void setDirection (Direction direction);
+	public Point getPostion ();
+	public Dimension getDimension ();
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int getSpeed() {
+		return Speed;
+	
+	}
+    public Image getImage ( ) {
+    	return Image;
+    }
+    public void move ();
+    public void placeInArea(Aera area) {}
+    public boolean isPlayer(int player) {}
+    public setDogfightModel(DogfightModel dogfightModel) {}
+    public default boolean hit() {
+		return false;}
+    public default boolean isWeapon() {
+		return false;}
+}
